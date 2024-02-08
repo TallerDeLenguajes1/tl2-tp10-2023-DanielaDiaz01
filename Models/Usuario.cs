9971b644265cfef975c0276a227f9e1c3a58136b
DanielaDiaz01 <dany.diaz1919@gmail.com>
2024-02-08 11:57:50 -0300

    primer commit

---
+++ Models/Usuario.cs
@@ -0,0 +1,9 @@
+namespace tl2_tp10_2023_DanielaDiaz01.Models
+{
+    public class Usuario{
+        int id;
+        string nombreUsuario;
+        public int Id { get => id; set => id = value; }
+        public string NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+    }
+}
